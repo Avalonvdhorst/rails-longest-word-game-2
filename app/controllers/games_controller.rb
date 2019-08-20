1e91dac3ef5fcaeba953d885e28dd0efce019ad7
Avalon van der Horst <avalonvdhorst@gmail.com>
2019-08-20 15:55:43 +0200

    rails longest word game include checking if a word is valid and contains the given letters

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,37 @@
+require 'open-uri'
+
+class GamesController < ApplicationController
+  def new
+    letters
+  end
+
+  def score
+    #raise
+    @letters = params[:letters].split(" ")
+    @word = params[:word].upcase
+    @included = include_word?(@letters, @word)
+    url = "https://wagon-dictionary.herokuapp.com/#{@word}"
+    wordhash = JSON.parse(open(url).read)
+    if wordhash["found"] && @included == true
+      @answer = "Congratulations! #{@word} is a perfect word!"
+    elsif @included == false
+      @answer = "Sorry but that can't be made from the given letters"
+    else
+      @answer = "Sorry #{@word} does not seem to be a valid English word"
+    # elsif include_word?(@letters, @word) == false
+    #   @answer = "Sorry but that can't be made from the given letters"
+    # else
+    #   @answer = "Sorry #{@word} does not seem to be a valid English word"
+    end
+  end
+
+  def include_word?(letters, word)
+    word.chars.all? { |letter| letters.include?(letter) }
+  end
+
+  private
+
+  def letters
+    @letters = ('A'..'Z').to_a.sample(10)
+  end
+end
